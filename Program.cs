b2648b38e41df13fc244b5485dda15818e4672de
GeorgeBennett3 <gbennett.personal@gmail.com>
2022-07-04 16:42:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace QueuePractice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<string> myQueue = new Queue<string>(3);
+            myQueue.Enqueue("Queues");
+            myQueue.Enqueue("Are");
+            myQueue.Enqueue("First in First out");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(myQueue.Dequeue());
+            };
+            Console.ReadLine();
+        }
+    }
+}
